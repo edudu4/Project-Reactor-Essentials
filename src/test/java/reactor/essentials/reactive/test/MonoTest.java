a8aaf6b8b9f0efdfc41400baa6ba09b0ae2e7ba6
eduardoalves4 <eduardo.rocha@placeti.com.br>
2024-02-14 17:16:30 -0300

    Quick Recap Reactive Streams

---
+++ src/test/java/reactor/essentials/reactive/test/MonoTest.java
@@ -0,0 +1,27 @@
+package reactor.essentials.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+/**
+ * Reactive Streams
+ * 1. Asynchronous
+ * 2. Non-blocking
+ * 3. Backpressure
+ * Publisher <- (subscribe) Subscriber
+ * Subscription is created
+ * Publisher (onSubscribe with the subscription) -> Subscriber
+ * Subscription <- (request N) Subscriber
+ * Publisher -> (onNext) Subscriber
+ * until:
+ * 1. Publisher sends all the objects requested.
+ * 2. Publisher sends all the objects it has. (onComplete) subscriber and subscription will be canceled.
+ * 3. There is an error. (onError) -> subscriber and subscription will be canceled.
+ */
+public class MonoTest {
+    @Test
+    public void test(){
+        log.info("Everything working as intended");
+    }
+}
